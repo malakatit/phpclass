5668b0d50e5da633965d9a44d8b6546558c3d346
Malak Atitallah <malak.atitallah@gmail.com>
2026-01-25 19:41:27 -0600

    :tada: Updated Public Website

--- index.php
+++ index.php
@@ -6,8 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
 <body>
-<h1>Test Homepage</h1>
+<header>
+    <h1>PHP Class - Malak Atitallah </h1>
+
+</header>
+<nav>
+    <ul>
+    <li><a href="#">Homepage</a> </li>
+    <li><a href="#">Loops</a> </li>
+    <li><a href="#">Countdown</a> </li>
+    <li><a href="#">Other</a> </li>
+    </ul>
+</nav>
+<main>
+    <img src="images/Spongebob.jpg">
+    <br/> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci aliquid beatae consequatur ea explicabo ipsam molestiae quaerat ratione voluptatum?Z
+    </p>
+</main>
+<footer>
+    <p>&copy; 2026 MA.com</p>
+</footer>
 </body>
 </html>
